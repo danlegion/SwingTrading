3664bc807213aa95c239fa4ac45ced82b30383c5
Daniel Ginez <daniel@payload.net>
2016-04-15 23:58:08 -0600

    adds lowers class

--- myfile.py
+++ myfile.py
@@ -19,3 +19,20 @@ def init():
     hist = share.get_historical('2016-02-01', '2016-04-15')
     hist = hist[::-1]
     swings(hist)
+
+
+
+class Lowers(object):
+    """docstring for Lowers"""
+    def __init__(self):
+        self.prev_low = 0
+        self.lowest = 0
+        self.drop_number = 0
+
+    def set_prev_low(self, new_low):
+        self.prev_low = new_low
+        if(self.prev_low < self.lowest):
+            self.lowest = self.prev_low
+
+#check for lower lows after seeing a swing up (3 times for higher confidence)
+#this will determine a down trend
