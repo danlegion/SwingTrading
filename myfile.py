a98073910af279d0e9bd22e0b658d53ca1d67fc1
Daniel Ginez <daniel@payload.net>
2016-04-18 09:11:14 -0600

    minor changes

--- myfile.py
+++ myfile.py
@@ -1,38 +1,56 @@
 from yahoo_finance import Share
 
-def swings(shares):
+
+def swings(shares, ll):
     i = 3
+    print ll.cur_low
     while i < len(shares):
-        if(shares[i] > shares[i-1] and shares[i-1] < shares[i-2]):
-            print "Swing Up " + shares[i]['Date']
-        elif(shares[i] < shares[i-1] and shares[i-1] > shares[i-2]):
-            print "Swing Down " + shares[i]['Date']
-        elif(shares[i] > shares[i-1]):
+        cur = shares[i]['Close']
+        prev = shares[i-1]['Close']
+        tprv = shares[i-2]['Close']
+        if(cur > prev and prev < tprv):
+            print "Swing Up\t" + shares[i]['Date']
+        elif(cur < prev and prev > tprv):
+            print "Swing Down\t" + shares[i]['Date']
+            analyzeDownTrend(cur, ll)
+        elif(cur > prev):
             print "Up"
-        elif(shares[i] < shares[i-1]):
-            print "Down"
+
+
+
         i += 1
+    print ll.cur_low
 
 
 def init():
     share = Share('YHOO')
-    hist = share.get_historical('2016-02-01', '2016-04-15')
+    hist = share.get_historical('2015-12-03', '2016-02-18')
     hist = hist[::-1]
-    swings(hist)
+    ll = Lowers(hist[3]['Close'])
+    swings(hist, ll)
 
+
+def analyzeDownTrend(share, lowers):
+    # print lowers.cur_low + "   cur: " + share
+    if lowers.cur_low > share:
+        lowers.drop_number += 1
+    if lowers.drop_number >= 3:
+        print "---------Lower Low"
+    lowers.set_prev_low(share)
 
 
 class Lowers(object):
     """docstring for Lowers"""
-    def __init__(self):
-        self.prev_low = 0
-        self.lowest = 0
+    def __init__(self, low):
+        self.cur_low = low
+        self.lowest = low
         self.drop_number = 0
+        self.confidence_level = 0
 
     def set_prev_low(self, new_low):
-        self.prev_low = new_low
-        if(self.prev_low < self.lowest):
-            self.lowest = self.prev_low
+        self.cur_low = new_low
+        if(self.cur_low < self.lowest):
+            self.lowest = self.cur_low
 
-#check for lower lows after seeing a swing up (3 times for higher confidence)
-#this will determine a down trend
+# check for lower lows after seeing a swing up (3 times for higher confidence)
+# this will determine a down trend
