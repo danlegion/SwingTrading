cfe3a717c11c10121a81029a81d35201a6e09bbf
Daniel G <dginez@gmail.com>
2017-01-12 11:23:34 -0700

    - makes stock dates changing

--- mysite/swing/views.py
+++ mysite/swing/views.py
@@ -3,15 +3,19 @@ from django.views import View
 from django.http import HttpResponse
 from logic.trends import Trend
 import json
+from datetime import date, timedelta
 
 
 class Swing(View):
 
+    # trends the stocks for the past 30 days
     def get(self, request):
+        today = str(date.today())
+        start_date = str(date.today() - timedelta(days=30))
         stocks = ['CVE.TO', 'GOOG', 'HBC.TO']
         data = []
         for symbol in stocks:
             share = Share(symbol)
-            values = share.get_historical('2016-12-01', '2017-01-11')
+            values = share.get_historical(start_date, today)
             data.append(Trend(values, symbol).toJson())
         return HttpResponse(json.dumps(data))
