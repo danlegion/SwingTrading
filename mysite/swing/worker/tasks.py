9498a010f69f19c3d3cab7ee62295251b93e09be
Daniel G <dginez@gmail.com>
2017-01-14 23:35:38 -0700

    switch to redistogo

--- mysite/swing/worker/tasks.py
+++ mysite/swing/worker/tasks.py
@@ -10,11 +10,12 @@ from datetime import timedelta
 from os import environ
 from swing.views import Swing
 
-REDIS_URL = environ.get('REDISCLOUD_URL', 'redis://localhost')
+REDIS_URL = environ.get('REDISTOGO_URL', 'redis://localhost')
 celery = Celery('tasks', broker=REDIS_URL)
 
 # @periodic_task(run_every=crontab(hour='6', minute=36))
 @periodic_task(run_every=timedelta(seconds=10))
 def runTrends():
+
     logging.info(Swing().analyze())
     # Swing().analyze()
