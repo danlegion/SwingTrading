486d25deee9bcd5c68af0a83e5da65231cdee029
Daniel G <dginez@gmail.com>
2017-01-14 22:43:39 -0700

    teste worker

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
 web: cd mysite && gunicorn mysite.wsgi:application -b 0.0.0.0:$PORT -w 10 --log-file -
 celery: python mysite/manage.py celeryd --events --loglevel=INFO -c 1 --settings=mysite.settings -B
+worker: cd mysite && celery -A tasks worker -B --loglevel=info
---
+++ mysite/tasks.py
@@ -0,0 +1,23 @@
+
+import logging
+
+from celery import Celery
+from celery.task import periodic_task
+from datetime import timedelta
+from os import environ
+
+REDIS_URL = environ.get('REDISTOGO_URL', 'redis://localhost')
+
+celery = Celery('tasks', broker=REDIS_URL)
+
+
+def fib(n):
+    if n > 1:
+        return fib(n - 1) + fib(n - 2)
+    else:
+        return 1
+
+
+@periodic_task(run_every=timedelta(seconds=10))
+def print_fib():
+    logging.info(fib(30))
