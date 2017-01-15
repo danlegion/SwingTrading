1fe0243a3178a4b6fcb8186b40684ce3c4264463
Daniel G <dginez@gmail.com>
2017-01-15 00:07:07 -0700

    adds sms notification

--- mysite/swing/worker/tasks.py
+++ mysite/swing/worker/tasks.py
@@ -10,20 +10,27 @@ from celery.schedules import crontab
 from datetime import timedelta
 from os import environ
 from swing.views import Swing
+import requests
 
 REDIS_URL = environ.get('REDISCLOUD_URL', 'redis://localhost')
 celery = Celery('tasks', broker=REDIS_URL)
 
-@periodic_task(run_every=timedelta(seconds=10))
+TILL_URL = environ.get("TILL_URL")
+
+@periodic_task(run_every=timedelta(seconds=20))
 # @periodic_task(run_every=crontab(hour='6', minute=36))
 def runTrends():
     logging.info("Stock analysis started...")
     trends = Swing().analyze()
 
     for trend in trends:
-        logging.info("Symbol: {0} Advice:{1}".format(trend.symbol, trend.notification))
+        news = "Symbol: {0} Advice:{1}".format(trend.symbol, trend.notification)
+        logging.info(news)
         if trend.report:
-            #send email here
-            pass
+            requests.post(TILL_URL, json={
+                "phone": ["15558675309", "15558675308"],
+                "text" : news
+            })
+
     logging.info("Stock analysis finished...")
 
