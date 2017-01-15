e3d783097faa36ef2f781146840856b2257538f9
Daniel G <dginez@gmail.com>
2017-01-14 23:40:39 -0700

    changes back to redis could redistogo not working

--- mysite/swing/worker/tasks.py
+++ mysite/swing/worker/tasks.py
@@ -10,7 +10,7 @@ from datetime import timedelta
 from os import environ
 from swing.views import Swing
 
-REDIS_URL = environ.get('REDISTOGO_URL', 'redis://localhost')
+REDIS_URL = environ.get('REDISCLOUD_URL', 'redis://localhost')
 celery = Celery('tasks', broker=REDIS_URL)
 
 # @periodic_task(run_every=crontab(hour='6', minute=36))
