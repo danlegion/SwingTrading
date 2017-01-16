60aa0d0c1db0631abab1e50ba985a6c81c546cae
Daniel G <dginez@gmail.com>
2017-01-15 21:56:08 -0700

    update crintab

--- mysite/swing/worker/tasks.py
+++ mysite/swing/worker/tasks.py
@@ -14,12 +14,15 @@ import requests
 
 REDIS_URL = environ.get('REDISCLOUD_URL', 'redis://localhost')
 celery = Celery('tasks', broker=REDIS_URL)
+celery.conf.update(
+    CELERY_TIMEZONE='Canada/Mountain'
+)
 
 TILL_URL = environ.get("TILL_URL")
 PHONE_NUMBER = environ.get("PHONE_NUMBER")
 
 # @periodic_task(run_every=timedelta(seconds=20))
-@periodic_task(run_every=crontab(hour='6', minute=36))
+@periodic_task(run_every=crontab(hour='21', minute=57))
 def runTrends():
     logging.info("Stock analysis started...")
     trends = Swing().analyze()
